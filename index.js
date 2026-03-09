b41dc5966d00d4481543b1e79cfcdfbd935262bd
saim-alam1 <allbehassansaim@gmail.com>
2026-03-09 20:49:27 +0600

    Installed Server Packages

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Role Nest Running");
+});
+
+app.listen(port, () => {
+  console.log("Role Nest Running:", port);
+});
